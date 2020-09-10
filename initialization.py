59e4da2875c1920300ba29fb0018465b0708bc73
Mosood Akeem <moshoodakeemolayinka@gmail.com>
2020-09-10 09:35:06 +0100

    'modifying'

--- initialization.py
+++ initialization.py
@@ -22,6 +22,7 @@ def index():
 	# name = ''
 	if form.validate_on_submit():
 		session['name'] = form.name.data
+		form.name.data = ''
 		return redirect(url_for('index'))
 	return render_template("index.html", form = form, name=session.get('name'))
 
