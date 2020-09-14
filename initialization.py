d5ceb29683a25564f9d5885b97d67e5b80ed32d1
Mosood Akeem <moshoodakeemolayinka@gmail.com>
2020-09-14 01:11:47 +0100

    'adding_flask_scripts'

--- initialization.py
+++ initialization.py
@@ -4,22 +4,29 @@ from flask_moment import Moment
 from datetime import datetime
 from form import NameForm
 from flask_sqlalchemy import SQLAlchemy
+from flask_script import Manager, Shell
+from flask_migrate import Migrate, MigrateCommand
 
 #initialization of Flask
 app = Flask(__name__)
+manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+
 
 app.config['SECRET_KEY'] = '0fb7ef58e49e4a7d83898e93ae3033e3'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db' 
 
 db = SQLAlchemy(app)
 
+migrate = Migrate(app,db)
+manager.add_command('db', MigrateCommand)
+
 class Role(db.Model):
 	__tablename__ = "roles" 
 	id = db.Column(db.Integer, primary_key = True)
 	name = db.Column(db.String , unique = True, nullable = False)
-	users = db.relationship("User", backref = "role", backref = 'dynamic')
+	users = db.relationship("User", backref = "role", lazy = 'dynamic')
 
 	def __repr__(self):
 		return f"Role {self.name}"
@@ -38,25 +45,25 @@ class User(db.Model):
 
 
 
-
-
-
-
-
-
 # Setting route & view function 
 @app.route("/", methods=['GET', 'POST']) 
 def index():
 	form = NameForm()
 	# name = ''
 	if form.validate_on_submit():
-		old_name = session.get('name')
-		if old_name is not None and old_name != form.name.data:
-			flash('username not matched, please check your spelling!')
+		user = User.query.filter_by(username=form.name.data).first()
+		if user is None:
+			user = User(username=form.name.data)
+			db.session.add(user)
+			db.session.commit()
+			session['known'] = False
+		else:
+			session['known'] = True
 		session['name'] = form.name.data
 		form.name.data = ''
 		return redirect(url_for('index'))
-	return render_template("index.html", form = form, name=session.get('name'))
+	return render_template("index.html", form = form, name=session.get('name'),
+																				known = session.get("known", False))
 
 @app.route("/user/<name>")  # setting dynamic route
 def user(name):
@@ -67,6 +74,17 @@ def user(name):
 def page_not_found(e):
 	return render_template('404.html'), 404
 
+
+def make_shell_context():
+	return dict(app=app, db=db, User=User, Role=Role)
+
+manager.add_command('shell', Shell(make_context = make_shell_context))
+
+
+
+
+
 # Server set up 
-if __name__ == "__main__":
-	app.run(debug=True)
+if __name__ == "__main__":    
+    manager.run()
+	# app.run(debug=True)																				
