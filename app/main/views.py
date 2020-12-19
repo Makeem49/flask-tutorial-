3947272f4f831a62d1c74a77e0f4a78c5005d0a0
Mosood Akeem <moshoodakeemolayinka@gmail.com>
2020-12-19 04:06:16 -0500

    adding editing and deleting route to authorize user

--- app/main/views.py
+++ app/main/views.py
@@ -8,7 +8,7 @@ from app.decorator import admin_required, permission_required
 from flask_login import login_required, current_user 
 from flask import abort 
 
- 
+
 
 @main.route('/', methods=['POST', 'GET'])  
 def index():
@@ -79,8 +79,32 @@ def edit_prof_admin():
 
 @main.route('/post/<int:id>', methods = ['GET', 'POST'])
 def post(id):
-	posts = Post.query.get_or_404(id)
-	return render_template('post.html', posts = [post])
+	post = Post.query.get_or_404(id)
+	return render_template('post.html', posts = [post], tiltle = 'post')
+
+@main.route('/edit/<int:id>', methods = ['GET', 'POST'])
+def edit(id):
+	post = Post.query.get_or_404(id)
+	if current_user != post.author and not current_user.can(Permission.ADMINISTER):
+		abort(403)
+	form  = PostForm()
+	if form.validate_on_submit():
+		post.body = form.body.data
+		db.session.add(post)
+		flash('The post has been updated')
+		return redirect(url_for('main.post', id = post.id))
+	form.body.data = post.body
+	return render_template('edit_post.html', form = form , title = 'Flask Edit Post')
+
+@main.route('/post/<int:id>/delete', methods = ['GET', 'POST'])
+def delete(id):
+	post = Post.query.get_or_404(id)
+	if current_user != post.author and not current_user.can(Permission.ADMINISTER):
+		abort(403)
+	db.session.delete(post)
+	db.session.commit()
+	return redirect(url_for('main.index'))
+	
 
 
 
